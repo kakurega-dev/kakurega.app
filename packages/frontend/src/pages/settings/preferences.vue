abe0ae8bf130571c47b49eec2e4c896a46fd3a4c
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-04-15 16:28:55 +0900

    refactor: 投稿フォーム関係の設定項目をpreferenceに統合

--- packages/frontend/src/pages/settings/preferences.vue
+++ packages/frontend/src/pages/settings/preferences.vue
@@ -139,10 +139,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 								</MkPreferenceContainer>
 							</SearchMarker>
 
-							<SearchMaker :keywords="['post', 'form']">
-								<FormLink to="/settings/post-form">{{ i18n.ts.postForm }}</FormLink>
-							</SearchMaker>
-
 							<SearchMarker :keywords="['renote']">
 								<MkPreferenceContainer k="collapseRenotes">
 									<MkSwitch v-model="collapseRenotes">
@@ -342,6 +338,72 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<template #icon><SearchIcon><i class="ti ti-edit"></i></SearchIcon></template>
 
 					<div class="_gaps_m">
+						<div class="_gaps_m">
+							<FormSlot>
+								<template #label>{{ i18n.ts.postForm }}<span class="_beta">{{ i18n.ts.originalFeature }}</span></template>
+								<MkContainer :showHeader="false">
+									<Sortable v-model="postFormItems" :class="$style.items" :itemKey="items => items" :animation="100" :delay="50" :delayOnTouchOnly="true">
+										<template #item="{element}">
+											<button v-tooltip="postformBottomItemDef[element.type].title" class="_button" :class="$style.item" @click="removeItemPostform(element.type, $event)">
+												<i class="ti ti-fw" :class="[$style.itemIcon, postformBottomItemDef[element.type].icon]"></i>
+											</button>
+										</template>
+									</Sortable>
+								</MkContainer>
+							</FormSlot>
+
+							<div class="_buttons">
+								<MkButton @click="addItemPostform"><i class="ti ti-plus"></i> {{ i18n.ts.addItem }}</MkButton>
+								<MkButton danger @click="resetPostform"><i class="ti ti-reload"></i> {{ i18n.ts.default }}</MkButton>
+								<MkButton primary class="save" @click="savePostform"><i class="ti ti-device-floppy"></i> {{ i18n.ts.save }}</MkButton>
+							</div>
+
+							<div :class="$style.label">{{ i18n.ts.postFormBottomSettingsDescription }}</div>
+						</div>
+
+						<hr>
+
+						<div class="_gaps_s">
+							<SearchMarker :keywords="['postform', 'draft', 'save']">
+								<MkSelect v-model="draftSavingBehavior">
+									<template #label><SearchLabel>{{ i18n.ts.draftSavingBehavior }}</SearchLabel><span class="_beta">{{ i18n.ts.originalFeature }}</span></template>
+									<option value="auto">{{ i18n.ts._draftSavingBehavior.auto }}</option>
+									<option value="manual">{{ i18n.ts._draftSavingBehavior.manual }}</option>
+								</MkSelect>
+							</SearchMarker>
+
+							<SearchMarker :keywords="['postform', 'draft']">
+								<MkSwitch v-model="disableNoteDrafting">
+									<template #caption><SearchKeyword>{{ i18n.ts.disableNoteDraftingDescription }}</SearchKeyword></template>
+									<SearchLabel>{{ i18n.ts.disableNoteDrafting }}</SearchLabel>
+									<span class="_beta">{{ i18n.ts.originalFeature }}</span>
+								</MkSwitch>
+							</SearchMarker>
+						</div>
+
+						<hr>
+
+						<div class="_gaps_s">
+							<SearchMarker :keywords="['postform', 'note', 'schedule', 'delete']">
+								<div>
+									<div :class="$style.label">
+										<SearchLabel>{{ i18n.ts.defaultScheduledNoteDeleteTime }}</SearchLabel>
+										<span class="_beta">{{ i18n.ts.originalFeature }}</span>
+									</div>
+									<MkDeleteScheduleEditor v-model="scheduledNoteDelete" :afterOnly="true"/>
+								</div>
+							</SearchMarker>
+
+							<SearchMarker :keywords="['postform', 'note', 'schedule', 'delete']">
+								<MkSwitch v-model="defaultScheduledNoteDelete">
+									<SearchLabel>{{ i18n.ts.defaultScheduledNoteDelete }}</SearchLabel>
+									<span class="_beta">{{ i18n.ts.originalFeature }}</span>
+								</MkSwitch>
+							</SearchMarker>
+						</div>
+
+						<hr>
+
 						<div class="_gaps_s">
 							<SearchMarker :keywords="['remember', 'keep', 'note', 'cw']">
 								<MkPreferenceContainer k="keepCw">
@@ -808,7 +870,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from 'vue';
+import { computed, defineAsyncComponent, ref, watch } from 'vue';
 import { langs } from '@@/js/config.js';
 import * as Misskey from 'misskey-js';
 import MkSwitch from '@/components/MkSwitch.vue';
@@ -821,17 +883,20 @@ import FormSection from '@/components/form/section.vue';
 import FormLink from '@/components/form/link.vue';
 import MkLink from '@/components/MkLink.vue';
 import MkInfo from '@/components/MkInfo.vue';
+import MkDeleteScheduleEditor from '@/components/MkDeleteScheduleEditor.vue';
 import MkDisableSection from '@/components/MkDisableSection.vue';
 import { store } from '@/store.js';
 import * as os from '@/os.js';
 import { misskeyApi } from '@/utility/misskey-api.js';
 import { reloadAsk } from '@/utility/reload-ask.js';
 import { isSupportNavigatorConnection } from '@/utility/datasaver.js';
+import { bottomItemDef as postformBottomItemDef } from '@/utility/post-form.js';
 import { fontList } from '@/utility/font.js';
 import { i18n } from '@/i18n.js';
 import { definePage } from '@/page.js';
 import { miLocalStorage } from '@/local-storage.js';
 import { prefer } from '@/preferences.js';
+import { PREF_DEF } from '@/preferences/def.js';
 import MkPreferenceContainer from '@/components/MkPreferenceContainer.vue';
 import MkFeatureBanner from '@/components/MkFeatureBanner.vue';
 import { globalEvents } from '@/events.js';
@@ -910,7 +975,11 @@ const enableOverrideTLDisplayLimit = prefer.model('enableOverrideTLDisplayLimit'
 const overrideTLDisplayLimit = prefer.model('overrideTLDisplayLimit');
 const menuStyle = prefer.model('menuStyle');
 const makeEveryTextElementsSelectable = prefer.model('makeEveryTextElementsSelectable');
+const disableNoteDrafting = prefer.model('disableNoteDrafting');
+const draftSavingBehavior = prefer.model('draftSavingBehavior');
+const defaultScheduledNoteDelete = prefer.model('defaultScheduledNoteDelete');
 
+const scheduledNoteDelete = ref({ deleteAt: null, deleteAfter: prefer.s.defaultScheduledNoteDeleteTime, isValid: true });
 const fontSize = ref(miLocalStorage.getItem('fontSize'));
 const useSystemFont = ref(miLocalStorage.getItem('useSystemFont') != null);
 
@@ -968,6 +1037,71 @@ watch([
 ], async () => {
 	await reloadAsk({ reason: i18n.ts.reloadToApplySetting, unison: true });
 });
+
+// #region PostFormCustomization
+watch(scheduledNoteDelete, () => {
+	if (!scheduledNoteDelete.value.isValid) return;
+	prefer.commit('defaultScheduledNoteDeleteTime', scheduledNoteDelete.value.deleteAfter);
+});
+
+const Sortable = defineAsyncComponent(() => import('vuedraggable').then(x => x.default));
+
+const postFormItems = ref(prefer.s.postFormActions.map(x => ({
+	id: Math.random().toString(),
+	type: x,
+})));
+
+async function addItemPostform() {
+	const currentItems = postFormItems.value.map(x => x.type);
+	const bottomItem = Object.keys(postformBottomItemDef).filter(k => !currentItems.includes(k));
+	const { canceled, result: item } = await os.select({
+		title: i18n.ts.addItem,
+		items: bottomItem.map(k => ({
+			value: k, text: postformBottomItemDef[k].title,
+		})),
+	});
+	if (canceled || item == null) return;
+	postFormItems.value = [...postFormItems.value, {
+		id: Math.random().toString(),
+		type: item,
+	}];
+}
+
+function getHTMLElement(ev: MouseEvent): HTMLElement {
+	const target = ev.currentTarget ?? ev.target;
+	return target as HTMLElement;
+}
+
+function removeItemPostform(type: keyof typeof postformBottomItemDef, ev: MouseEvent) {
+	const item = postformBottomItemDef[type];
+	os.popupMenu([{
+		type: 'label',
+		text: item.title,
+	}, {
+		text: i18n.ts.remove,
+		action: () => {
+			postFormItems.value = postFormItems.value.filter(x => x.type !== type);
+		},
+	}], getHTMLElement(ev));
+}
+
+async function savePostform() {
+	prefer.commit('postFormActions', postFormItems.value.map(x => x.type));
+}
+
+async function resetPostform() {
+	const result = await os.confirm({
+		type: 'warning',
+		text: i18n.ts.resetAreYouSure,
+	});
+	if (result.canceled) return;
+
+	postFormItems.value = PREF_DEF.postFormActions.default.map(x => ({
+		id: Math.random().toString(),
+		type: x,
+	}));
+}
+// #endregion
 
 const emojiIndexLangs = ['en-US', 'ja-JP', 'ja-JP_hira'] as const;
 
@@ -1089,3 +1223,36 @@ definePage(() => ({
 	icon: 'ti ti-adjustments',
 }));
 </script>
+
+<style lang="scss" module>
+.items {
+	padding: 8px;
+	flex: 1;
+	display: grid;
+	grid-auto-flow: row;
+	grid-template-columns: repeat(auto-fill, minmax(42px, 1fr));
+	grid-auto-rows: 40px;
+	background: var(--MI_THEME-buttonBg);
+	border-radius: 12px;
+}
+
+.item {
+	display: inline-block;
+	padding: 0;
+	margin: 0;
+	font-size: 1em;
+	width: auto;
+	height: 100%;
+	border-radius: 6px;
+
+	&:hover {
+		background: var(--MI_THEME-buttonHoverBg);
+	}
+}
+
+.label {
+	font-size: 0.85em;
+	padding: 0 0 8px 0;
+	user-select: none;
+}
+</style>
