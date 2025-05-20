20904732e1ec4433768f3eba593e50ccbbea18ec
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-05-20 18:27:13 +0900

    enhance: 登録フォームのデザインを改善

--- packages/frontend/src/components/MkSignupDialog.rules.vue
+++ packages/frontend/src/components/MkSignupDialog.rules.vue
@@ -10,13 +10,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 	</div>
 	<div class="_spacer" style="--MI_SPACER-min: 20px; --MI_SPACER-max: 28px;">
 		<div class="_gaps_m">
-			<div v-if="instance.disableRegistration || instance.federation !== 'all'" class="_gaps_s">
-				<MkInfo v-if="instance.disableRegistration" warn>{{ i18n.ts.invitationRequiredToRegister }}</MkInfo>
+			<div v-if="instance.disableRegistration || instance.enableRegistrationLimit || instance.federation !== 'all'" class="_gaps_s">
+				<MkInfo v-if="instance.enableRegistrationLimit" warn>{{ i18n.ts.registrationLimitEnabled }}</MkInfo>
+				<MkInfo v-else-if="instance.disableRegistration" warn>{{ i18n.ts.invitationRequiredToRegister }}</MkInfo>
 				<MkInfo v-if="instance.federation === 'specified'" warn>{{ i18n.ts.federationSpecified }}</MkInfo>
 				<MkInfo v-else-if="instance.federation === 'none'" warn>{{ i18n.ts.federationDisabled }}</MkInfo>
-			</div>
-			<div v-if="instance.enableRegistrationLimit" :class="$style.mainWarn">
-				<MkInfo warn>{{ i18n.ts.registrationLimitEnabled }}</MkInfo>
 			</div>
 
 			<div style="text-align: center;">
